7b397815d1fab188951950f6986adba5db9c7480
Dylan Turnbull <dylanturn@gmail.com>
2020-10-06 08:20:30 -0500

    comment out stuff we wont be needing

--- variables.tf
+++ variables.tf
@@ -43,35 +43,35 @@ variable "cluster_node_groups" {
   }))
 }
 
-variable "cluster_services" {
-  type = object({
-    cert_manager : object({
-
-    })
-    traefik : object({
-      namespace : string,
-      log_level : string,
-      replicas : number,
-      rolling_update_max_surge : number,
-      rolling_update_max_unavailable : number,
-      pod_termination_grace_period_seconds : number
-    })
-    # With any luck we'll be able to default `argocd` to null here. Until then we'll just have to do that in the calling module.
-    argocd : object({
-      namespace : string,
-      server_replicas : number,
-      repo_replicas : number,
-      enable_dex : bool,
-      enable_ha_redis : bool,
-      oidc_name : string,
-      oidc_issuer : string,
-      oidc_client_id : string,
-      oidc_client_secret : string,
-      oidc_requested_scopes : list(string),
-      oidc_requested_id_token_claims : map(any)
-    })
-  })
-}
+#ariable "cluster_services" {
+# type = object({
+#   cert_manager : object({
+#
+#   })
+#   traefik : object({
+#     namespace : string,
+#     log_level : string,
+#     replicas : number,
+#     rolling_update_max_surge : number,
+#     rolling_update_max_unavailable : number,
+#     pod_termination_grace_period_seconds : number
+#   })
+#   # With any luck we'll be able to default `argocd` to null here. Until then we'll just have to do that in the calling module.
+#   argocd : object({
+#     namespace : string,
+#     server_replicas : number,
+#     repo_replicas : number,
+#     enable_dex : bool,
+#     enable_ha_redis : bool,
+#     oidc_name : string,
+#     oidc_issuer : string,
+#     oidc_client_id : string,
+#     oidc_client_secret : string,
+#     oidc_requested_scopes : list(string),
+#     oidc_requested_id_token_claims : map(any)
+#   })
+# })
+#
 
 variable "tags" {
   type = list(string)
